7b7e602feed7bf08ef67dfce49d338269ba8484e
Mukul Agrawal <mukul2310@github.com>
2021-05-21 17:35:42 +0530

    basic cd

--- shell.c
+++ shell.c
@@ -1,6 +1,31 @@
 #include<stdio.h>
+#include<string.h>
+void show_prompt()
+{
+    printf("$");
+}
+int valid_command(char s[])
+{
+    return !strcmp(s,"cd");
+}
 int main(int argc, char const *argv[])
 {
-    printf("Hello World");
+    char s[100];
+    do
+    {
+        //show prompt
+        show_prompt();
+        //read command
+        fgets(s,1000,stdin);
+        s[strlen(s)-1]='\0';
+        // tell if it is valid or not
+        if(valid_command(s))
+            printf("valid\n");
+        else
+        {
+            printf("shell: %s: command not found\n",s);
+            exit(0);
+        }
+    } while (s[0]!='\n');
     return 0;
 }
